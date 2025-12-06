f1e52c301e31fee2560bd22cc2e12d85184be93d
SKJ0001 <ksjeong321@snu.ac.kr>
2025-12-06 18:00:11 +0900

    Fix: TagCreator.tsx가 없어서 발생하는 오류를 임시로 처리

---
+++ src/design/components/TagCreator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// 에러 방지용 임시 컴포넌트입니다.
+export const TagCreator = () => {
+  return (
+    <div className="p-4 border border-dashed border-gray-400">
+      태그 생성기 (TagCreator) 컴포넌트가 없습니다.
+    </div>
+  );
+};
