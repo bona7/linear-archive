46eba307c01a341b9b616bb82dde191177df4f7d
minseok05423 <106617123+minseok05423@users.noreply.github.com>
2025-12-13 08:14:01 +0900

    feat: added statistical analysis and agentic rag functionalities in ai summarization

--- src/commons/statistics/tags.ts
+++
@@ -1,9 +0,0 @@
-function tagInsertCallback (payload: unknown) {
-    console.log("tagInsertCallback", payload);
-}
-
-function tagDeleteCallback (payload: unknown) {
-    console.log("tagDeleteCallback", payload);
-}
-
-export {tagInsertCallback, tagDeleteCallback}
